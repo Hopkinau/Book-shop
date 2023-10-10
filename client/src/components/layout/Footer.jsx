48925e72fd3e65e32c3d277ebb75f597f305b5c8
PENG HE <hopkinau@gmail.com>
2023-10-11 10:37:56 +1100

    footer update

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -1,14 +1,37 @@
 import * as styles from './Footer.css';
 
-const Footer = () => {
-  // Dynamic Date Function
-  const getCurrentYear = () => {
-    return new Date().getFullYear();
-  };
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
 
+function Footer() {
   return (
-    <div className={styles.footer}>&copy; {getCurrentYear()} Blue Mountain</div>
+    <div className={styles.footer}>
+      <p className={styles.paragraph}>Dont miss the latest Sales</p>
+      <Form>
+        <Row className='align-items-center'>
+          <Col xs='auto'>
+            <Form.Label htmlFor='inlineFormInput' visuallyHidden>
+              Name
+            </Form.Label>
+            <Form.Control
+              className='mb-2'
+              id='inlineFormInput'
+              placeholder='Jane Doe'
+            />
+          </Col>
+
+          <Col xs='auto'>
+            <Button type='submit' className='mb-2'>
+              Submit
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    </div>
   );
-};
+}
 
 export default Footer;
