bd47a37d98704d66afcd545319ddc8efe791e36a
PENG HE <hopkinau@gmail.com>
2023-10-15 23:05:05 +1100

    Update Card.css.ts

--- client/src/components/features/cart/Card.css.ts
+++ client/src/components/features/cart/Card.css.ts
@@ -2,4 +2,9 @@ import { style } from '@vanilla-extract/css';
 import { vars } from '../../../styles/themes.css';
 export const card = style({
   display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  padding: '0.5rem 1rem',
+  margin: '1.5rem',
 });
