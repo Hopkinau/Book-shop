9bddcf2f4e86ab8404b4c08be4dff3021c264eb5
PENG HE <hopkinau@gmail.com>
2023-10-27 23:23:14 +1100

    Update fileServerUpload.js

--- server/src/middleware/fileServerUpload.js
+++ server/src/middleware/fileServerUpload.js
@@ -4,6 +4,7 @@ const path = require('path');
 const debugWRITE = require('debug')('app:write');
 //piece of middle just of function
 const fileServerUpload = (req, res, next) => {
+  //1.check file has been uploaded
   if (req.files) {
     const file = req.files.image;
     debugWRITE(`Image:${file.name} received`);
