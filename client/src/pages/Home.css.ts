00dafef12810e55a1b8d037e87cf51e854671176
PENG HE <hopkinau@gmail.com>
2023-11-03 23:16:45 +1100

    Update Home.css.ts

--- client/src/pages/Home.css.ts
+++ client/src/pages/Home.css.ts
@@ -3,9 +3,8 @@ import { vars } from '../styles/themes.css';
 
 export const bgImage = style({
   position: 'relative',
-
   width: '100vw',
-  height: 'auto',
+  height: '20vh',
   overflow: 'hidden',
   display: 'flex',
   alignItems: 'center',
