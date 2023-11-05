32701ac195c37428fb02f23959b34c65ced39954
PENG HE <hopkinau@gmail.com>
2023-11-06 07:06:46 +1100

    Update filePolicy.js

--- server/src/policies/filePolicy.js
+++ server/src/policies/filePolicy.js
@@ -1,42 +1,56 @@
 const ApiError = require('../utilities/ApiError');
 const path = require('path');
-//The filePolicy is a piece of middleware that validator the file upload
 
-//1.check file exists
-const filePayloadExists = (req, res, next) => {
-  if (!req.files && !req.body.uploadFile) {
-    return next(ApiError.badRequest('No file was uploaded'));
+// [1] VALIDATION: Check for file passed from client
+const filesPayloadExists = (req, res, next) => {
+  if (!req.files && !req.body.uploadedFile) {
+    return next(ApiError.badRequest('No file uploaded'));
   }
   next();
 };
-//2.check file size
+
+// [2] VALIDATION: Check if file size exceeds set size
 const fileSizeLimiter = (req, res, next) => {
-  const MB = 5; // 5MB limit
+  const MB = 5; // 5MB
   const FILE_SIZE_LIMIT = MB * 1024 * 1024;
+
   if (req.files) {
     const file = req.files.image;
+
     if (file.size > FILE_SIZE_LIMIT) {
+      const message = `${file.name.toString()} is over the file size limit of ${MB} MB.`;
+
       return next(ApiError.tooLarge(message));
     }
-    next();
   }
+  next();
 };
-//3.check file extension/type
+
+// [3] VALIDATION: Restrict file to accepted file extension types (images ONLY)
 const fileExtLimiter = (allowedExtArray) => {
   return (req, res, next) => {
     if (req.files) {
       const file = req.files.image;
       const fileExtension = path.extname(file.name);
-      if (!allowedExtArray.includes(fileExtension)) {
-        return next(ApiError.badRequest('File type not allowed'));
+
+      const allowed = allowedExtArray.includes(fileExtension);
+      if (!allowed) {
+        const message =
+          `Only ${allowedExtArray.toString()} files allowed.`.replaceAll(
+            ',',
+            ', '
+          );
+
+        return next(ApiError.cannotProcess(message));
       }
     }
+
     next();
   };
 };
 
 const filePolicy = {
-  filePayloadExists,
+  filesPayloadExists,
   fileSizeLimiter,
   fileExtLimiter,
 };
