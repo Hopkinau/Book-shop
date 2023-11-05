206dcf68485fbce969ee82c9dee836424f41ffc7
PENG HE <hopkinau@gmail.com>
2023-11-05 20:25:40 +1100

    delete not use data

--- client/src/components/features/cart/HomeButton.css.ts
+++
@@ -1,9 +0,0 @@
-import { style } from '@vanilla-extract/css';
-import { vars } from '../../../styles/themes.css';
-export const title = style({
-  display: 'flex',
-  position: 'absolute',
-  top: '250px',
-  right: '20px',
-  zIndex: 1,
-});
