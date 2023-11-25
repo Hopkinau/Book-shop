3416e570d2cc4d431229c0506e71fa14380dc12f
PENG HE <hopkinau@gmail.com>
2023-11-25 18:28:51 +1100

    update the config

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,10 +1,10 @@
 module.exports = {
   // [A] PORT ENVs
   port: process.env.PORT,
-  
+
   // [B] DATABASE ENVs
   db: {
-    serviceAccountKey: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+    google_account_credentials: process.env.GOOGLE_APPLICATION_CREDENTIALS,
     storageBucket: process.env.STORAGE_BUCKET_URL,
   },
 
@@ -17,6 +17,6 @@ module.exports = {
   // [D] Approved CORS root URLs
   corsAllowedOptions: [
     process.env.CORS_WHITELIST_1,
-    process.env.CORS_WHITELIST_2
-  ]  
-}
+    process.env.CORS_WHITELIST_2,
+  ],
+};
--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,7 +1,7 @@
 // DATABASE CONFIGURATION
 // Imports of db admin modules (imports the admin libraries)
-var admin = require("firebase-admin");
-const config = require("./config")
+var admin = require('firebase-admin');
+const config = require('./config');
 
 // Import debug logs
 const dbStartup = require('debug')('app:db');
@@ -9,33 +9,31 @@ const debugError500 = require('debug')('app:error500');
 
 try {
   dbStartup('Attempting database connection...');
-  // Imports of db credentials
-  var serviceAccount = require(config.db.serviceAccountKey);
-  
-  // Configure database with our credentials + storage bucket details (for file/image storing)
+
+  // Setup of db credential and options
+
+  // Initialize firebase services
   admin.initializeApp({
     // Set Application Default Credentials (ADC implicitly determines credentials from GOOGLE_APPLICATION_CREDENTIALS ENV)
-    credential: admin.credential.cert(serviceAccount),
-    storageBucket: config.db.storageBucket
+    credential: admin.credential.cert(config.db.google_account_credentials),
+    storageBucket: config.db.storageBucket,
   });
-  
-  // Store core database functions in variable objects (these each represent its OWN API, as part of the wider admin SDK!)
+
   const db = admin.firestore();
   const bucket = admin.storage().bucket();
 
-  // DB Ping function
-  const dbPing = db.listCollections()
-  .then(collections => {
-    dbStartup("Connected to Cloud Firestore");
-    for (let collection of collections) {
-      dbStartup(`Found db collection: ${collection.id}`);
-    }
-  });
+  // DB Ping function just for testing
+  // const dbPing = db.listCollections().then((collections) => {
+  //   dbStartup('Connected to Cloud Firestore');
+  //   for (let collection of collections) {
+  //     dbStartup(`Found db collection: ${collection.id}`);
+  //   }
+  // });
 
   // Export variable objects for use in our application
   module.exports = { db, bucket, dbPing };
 
-// DEBUG: Unhandled error will be logged to console
-} catch(err) {
+  // DEBUG: Unhandled error will be logged to console
+} catch (err) {
   debugError500(err);
 }
