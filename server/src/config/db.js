fd7778e9fcb815bcb4cc4437c5394655638e04c9
PENG HE <hopkinau@gmail.com>
2023-11-26 08:33:09 +1100

    update the config file for hosting

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -9,8 +9,28 @@ const debugError500 = require('debug')('app:error500');
 
 try {
   dbStartup('Attempting database connection...');
-
   // Setup of db credential and options
+
+  //1.STANDARD SETUP:ENV KEY
+  let serviceAccountKey;
+  if (config.env === 'development' || config.env === 'production') {
+    serviceAccountKey = config.db.google_account_credentials;
+
+    //2.NEW SETUP:SEPARATE ENVS
+  } else if (config.env === 'preview') {
+    serviceAccountKey = {
+      type: config.db.type,
+      project_id: config.db.project_id,
+      private_key_id: config.db.private_key_id,
+      private_key: config.db.private_key,
+      client_email: config.db.client_email,
+      client_id: config.db.client_id,
+      auth_uri: config.db.auth_uri,
+      token_uri: config.db.token_uri,
+      auth_provider_x509_cert_url: config.db.auth_provider_x509_cert_url,
+      client_x509_cert_url: config.db.client_x509_cert_url,
+      universe_domain: config.db.universe_domain,
+  }
 
   // Initialize firebase services
   admin.initializeApp({
